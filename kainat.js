9ab672a675dcf46d466fc62fa3b1301331cc4110
GrandIntrusion123 <kainatpachakhail@students.berkeley.net>
2017-05-19 10:14:54 -0700

    Update kainat.js

--- kainat.js
+++ kainat.js
@@ -1,3 +1,18 @@
 /**
  * Created by h205p2 on 5/18/17.
  */
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Get ready to play...</p>
+
+<div>
+  <h3>Trivia!</h3>
+  <p>Can you answer all questions correctly before the time runs out?</p>
+</div>
+
+<p>Let's go!</p>
+
+</body>
+</html>
